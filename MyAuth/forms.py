b1af8b2429c37460bc14035bcb2eb1139953177f
Aarati123456 <aaratiangbuhang2058@gmail.com>
2025-07-09 17:13:10 +0545

    This is the final project of my Auth

--- MyAuth/forms.py
+++ MyAuth/forms.py
@@ -1,43 +1,43 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.admin.models import User 
+from django.contrib.auth.models import User
 
-
-class customUserCreationform (UserCreationForm): 
+class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
-    first_name = forms.CharField(max_length=40,required=True)
-    last_name = forms.CharField(max_length=40,required=True)
+    first_name = forms.CharField(max_length=40, required=True)
+    last_name = forms.CharField(max_length=40, required=True)
     
     class Meta:
         model = User
-        fields = ('username','email','first_name','last_name','password1','password2')
+        fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2')
         
-        def save(self, commit = True):
-            user = super().save(commit=False)
-            User.email = self.cleaned_data['email']
-            user.first_name = self.cleaned_data['first_name']
-            user.last_name = self.cleaned_data['last_name']
-            if commit:
-                user.save()
-                return user 
-            
-class CustomerAuthenticationForm(AuthenticationForm):
-    pass
+    def save(self, commit = True):
+        user = super().save(commit=False)
+        user.email= self.cleaned_data['email']
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        if commit:
+            user.save()
+        return user
+    
+class CustomAuthenticationForm(AuthenticationForm):
+    pass 
 
-            
-class CustomRegisterForm( forms.Form):
+
+
+class CustomRegisterForm(forms.Form):
     username = forms.CharField(max_length=150, required=True)
     email = forms.EmailField(required=True)
-    first_name = forms.Charfield(max_length=50, required=True) 
-    last_name = forms.CharField(max_length=50, required=True) 
-    password1 = forms.CharField(label = 'password',widget = forms.passwordInput, required=True)  
-    password2 = forms.CharField(label = 'password',widget = forms.passwordInput, required=True)      
+    first_name = forms.CharField(max_length=50, required=True)
+    last_name = forms.CharField(max_length=50, required=True)
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput, required=True)
+    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput, required=True)
     
     def clean_username(self):
-         username= self.cleaned_data ['username']
-         if  User.objects.filter(username=username).exists():
-            raise forms.ValidationError('Username already exists') 
-         return username
+        username= self.cleaned_data['username']
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError('Username already exists')
+        return username
     
     def clean_email(self):
         email = self.cleaned_data['email']
@@ -45,14 +45,14 @@ class CustomRegisterForm( forms.Form):
             raise forms.ValidationError('Email already exists')
         return email
     
-    def clean(self):    
-        cleaned_data= super(). clean()
-        password1= cleaned_data.get('password1')
-        password2= cleaned_data.get('password2')
+    def clean(self):
+        cleaned_data = super().clean()
+        password1 = cleaned_data.get('password1')
+        password2 = cleaned_data.get('password2')
         if password1 and password2 and password1 != password2:
-           raise forms.ValidationError("password doesnot match")
+            raise forms.ValidationError("Password doesnt match")
         return cleaned_data
     
-    class CustomLoginForm(forms.Form):
-     username = forms.CharField (max_length=150, required=True)
-    password = forms.CharField (widget=forms.PasswordInput, required=True)
+class CustomLoginForm(forms.Form):
+    username = forms.CharField(max_length=150, required=True)
+    password = forms.CharField(widget=forms.PasswordInput, required=True)
--- MyAuth/urls.py
+++ MyAuth/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('', views.home, name='home'),
-    path('custom_register/', views.custom_register, name='custom_register')
+    path('custom_register/', views.custom_register, name='custom_register'),
+    path ('custom_login/', views.custom_login, name='custom_login')
     
 ]
